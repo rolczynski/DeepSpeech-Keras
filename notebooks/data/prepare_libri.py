6abebff2a4a732a523d65b416f46c6a92b3580b5
Qbit- <rss3@rice.edu>
2020-05-25 16:05:42 +0300

    New dataset preparation script

--- notebooks/data/prepare_libri.py
+++ notebooks/data/prepare_libri.py
@@ -11,13 +11,11 @@ import tempfile
 logging.basicConfig(level=logging.INFO)
 
 datasets = {
-    'dev': {
+    'dev-clean': {
         'url': 'http://www.openslr.org/resources/12/dev-clean.tar.gz',
-        'result_folder': 'libri_dev'
     },
-    'test': {
+    'test-clean': {
         'url': 'http://www.openslr.org/resources/12/test-clean.tar.gz',
-        'result_folder': 'libri_test'
     }
 }
 
@@ -29,7 +27,7 @@ def read_transcript(filename: str) -> List[Tuple[str, str]]:
     :return:
     """
     with open(filename, 'r') as file:
-        result = [tuple(line.strip().split(' ', 1).lower())
+        result = [tuple(line.strip().lower().split(' ', 1))
                   for line in file.readlines()]
     return result
 
@@ -77,55 +75,131 @@ def transfer_transcripted_audio(
     return transcripts, file_sizes
 
 
-def main(ds_name: str, audio_path_prefix: str):
-    url = datasets[ds_name]['url']
-    result_folder = datasets[ds_name]['result_folder']
+def create_index_data(cwd: str, dataset_path: str) -> pd.DataFrame:
+    """
+    Creates the index file which is suitable for the pipeline.
+    The file contains paths to audiofiles and the transcripts
 
-    # tar_name = f'{ds_folder_name}.tar.gz'
+    :param cwd: current work directory.
+                All paths will be relative to this directory.
+    :param dataset_path: path of the dataset. May be either absolute or
+                relative
+    :return: pandas DataFrame with path, transcript and file size
+    """
+    prefix = os.path.relpath(dataset_path, start=cwd)
+
+    def walk_dirs(current_folder: str):
+        file_paths = []
+        transcripts = []
+        file_sizes = []
+        for item in os.listdir(current_folder):
+            if os.path.isdir(os.path.join(current_folder, item)):
+                # Go recursive
+                (item_file_paths,
+                 item_transcripts, item_filesizes) = walk_dirs(
+                     os.path.join(current_folder, item))
+
+                file_paths.extend(item_file_paths)
+                transcripts.extend(item_transcripts)
+                file_sizes.extend(item_filesizes)
+
+            elif item.endswith('.trans.txt'):
+                # Read transcript
+                item_file_paths, item_transcripts = zip(*read_transcript(
+                    os.path.join(current_folder, item)))
+                for file_name in item_file_paths:
+                    file_paths.append(
+                        os.path.relpath(
+                            os.path.join(
+                                current_folder, f'{file_name}.wav'),
+                            start=cwd)
+                        )
+                    file_sizes.append(
+                        os.path.getsize(
+                            os.path.join(
+                                current_folder, f'{file_name}.wav')
+                        )
+                    )
+                    transcripts.extend(item_transcripts)
+        return file_paths, transcripts, file_sizes
+
+    file_paths, transcripts, file_sizes = walk_dirs(dataset_path)
+    index_data = pd.DataFrame(
+        zip(file_paths, transcripts, file_sizes),
+        columns=['path', 'transcript', 'file_size'])
+
+    return index_data
+
+
+def transcode_flac_wav_recursive(path, keep_original=False):
+    """
+    For any FLAC file found in path creates a corresponding WAV file
+    :param path: entry point
+    :param keep_original: if original FLAC file should be kept
+    :return: None if successfull
+    """
+    for item in os.listdir(path):
+        if os.path.isdir(os.path.join(path, item)):
+            transcode_flac_wav_recursive(
+                os.path.join(path, item), keep_original=keep_original)
+        elif item.endswith('.flac'):
+            convert_flac_to_wav(
+                os.path.join(path, item),
+                os.path.join(path, os.path.splitext(item)[0] + '.wav')
+            )
+            if not keep_original:
+                os.remove(os.path.join(path, item))
+
+
+def main(ds_name: str, cwd: str, dest_dir: str):
+    """
+    :param ds_name: name of the dataset to use
+    :param cwd: current work dir, where index will be placed
+    :param dest_dir: location where the dataset will be placed
+    """
+    url = datasets[ds_name]['url']
+
     tar_file_name = url.split('/')[-1]
-    audio_data_dir = tar_file_name.strip('.tar.gz')
+    audio_data_dir = tar_file_name.split('.', 1)[0]
 
     # Prepare main dataset
-    if not os.path.isfile(tar_file_name):
-        logging.info(f'Not found tar file {tar_file_name}.'
-                     ' Downloading it.')
-        urllib.request.urlretrieve(url, tar_file_name)
-        logging.info(f'Successfully downloaded tar file.')
+    if not os.path.isdir(
+            os.path.join(dest_dir, 'LibriSpeech', audio_data_dir)):
+        if not os.path.isfile(tar_file_name):
+            logging.info(f'Not found tar file {tar_file_name}.'
+                         ' Downloading it.')
+            urllib.request.urlretrieve(url, tar_file_name)
+            logging.info(f'Successfully downloaded tar file.')
 
-    with tempfile.TemporaryDirectory(prefix='.') as tmp_dir:
-        logging.info(f'Extracting into {tmp_dir}')
+        logging.info(f'Extracting into {dest_dir}')
         tar = tarfile.open(tar_file_name, "r:gz")
-        tar.extractall(tmp_dir)
+        tar.extractall(dest_dir)
         tar.close()
 
-        # Restructure dataset to our format
-        if not os.path.isdir(result_folder):
-            os.mkdir(result_folder)
-            logging.info('Starting to convert all audio files and gather'
-                         ' transcripts.')
-            all_transcripts, all_filesizes = transfer_transcripted_audio(
-                f'{tmp_dir}/LibriSpeech/{audio_data_dir}',
-                result_folder)
+    # Transcode FLAC to WAV and create an index
+    logging.info('Transcoding FLAC files')
+    transcode_flac_wav_recursive(
+        os.path.join(dest_dir, 'LibriSpeech', audio_data_dir))
 
-    logging.info('Saving transcripts int data.csv.')
-    transcripts_df = pd.DataFrame(
-        all_transcripts, columns=['path', 'transcript'])
-    transcripts_df['filesize'] = all_filesizes
-    transcripts_df.path = audio_path_prefix + f"/{result_folder}/" +\
-        transcripts_df.path + ".wav"
-    transcripts_df.to_csv(f'{result_folder}/data.csv')
+    logging.info('Generating index data')
+    index_data = create_index_data(
+        cwd, os.path.join(dest_dir, 'LibriSpeech', audio_data_dir))
+    index_data.to_csv(os.path.join(cwd, f'{ds_name}-index.csv'))
 
 
 if __name__ == '__main__':
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
     parser = argparse.ArgumentParser(
         description='Prepare LibriSpeech data')
     parser.add_argument('--type', type=str,
                         help='which dataset to download',
-                        default='dev',
+                        default='dev-clean',
                         choices=datasets.keys())
-    parser.add_argument('--prefix', type=str,
-                        help='prefix to add before all audio paths',
+    parser.add_argument('--dest', type=str,
+                        help='where to place final dataset',
+                        default='.')
+    parser.add_argument('--index_top', type=str,
+                        help='path relative to which all'
+                             'audio paths will be indexed',
                         default='.')
     args = parser.parse_args()
-    main(args.type, args.prefix)
+    main(args.type, args.index_top, args.dest)
